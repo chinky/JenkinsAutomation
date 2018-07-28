fd7f56290378924d464222a00367fe7b954bc2bc
chinkyloomba <chinky.loomba@gemalto.com>
2018-07-28 12:21:40 +0530

    Added test files for local Jenkins

---
+++ Hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+
+
+int main() {
+
+	cout<<" Entering a  sample program for git/ jenkins integration";
+
+	for(int k =0; k< 10; k++) {
+
+		cout<<"Inside loop"<<"Hello world"<<k<<"\n";
+
+	}
+	
+}
